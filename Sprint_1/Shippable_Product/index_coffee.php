5f5765b8264669ad148a029fed19e9225822862d
Larry Ho <102478827+LarryHo-Building@users.noreply.github.com>
2023-05-02 13:40:24 +1000

    Sprint 1

---
+++ Sprint_1/Shippable_Product/index_coffee.php
@@ -0,0 +1,78 @@
+<?php
+        require_once("4U_tools.php");
+        top_module("Coffee");
+    ?>
+<style type="text/css">
+.box-wrap {
+    text-align: center;
+    background-color: #d4d4f5;
+    
+}
+
+.box-wrap>div {
+    width: 23%;
+   
+    margin: 1%;
+    border-radius: 10%;
+    float: left;
+	
+   
+}
+
+  
+        
+</style>
+
+    
+	
+	<body>
+	        
+
+        <img src="img/index-header.jpg" class="image-header-div" />
+		
+		<div align="center">
+		<a href="index_coffee.php">
+		   <button>coffee</button>
+		</a>
+		<a href="index_cake.php">
+		   <button>cake</button>
+		</a>
+		<a href="index_sandswich.php">
+		   <button>sandswich</button>
+		</a>
+		</div>
+		
+		<div class="box-wrap">
+		<?php
+		
+		 foreach(file('4U.txt') as $line){
+			 $array = explode(",", $line);
+			 $Item_id=$array[0];
+			 $Item_name=$array[1];
+			 $Item_type=$array[2];
+			 $base_price=$array[3];
+			 $image_url=$array[4];
+			
+			 if ($Item_id!='Item_id' && $Item_type=='Coffee'){
+			echo '<div class="box">';
+			echo'<img src="'.$image_url.'" class="image-pic1" />';
+			echo '<label>'.$Item_name.'</label>';
+			 echo '</div>';
+			 }
+
+			 
+		
+		 }
+	    ?>	
+		</div>
+       
+        
+		
+
+
+	</body>
+    
+  <?php   
+        require_once("4U_tools.php");
+        end_module("");
+    ?>
